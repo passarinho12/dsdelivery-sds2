c5843c5e1f887be7517acc9cc3245c90e56b7e5c
unknown <nathan.almeida2001@hotmail.com>
2021-01-08 19:17:35 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="http://youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://linkedin.com/school/devsuperior/" target="_new">
+                <LinkedinIcon />
+                </a>
+                <a href="http://instagram.com/devsuperior.ig"> </a>
+                <InstagramIcon />
+            </div>
+        </footer>
+
+
+    )
+
+
+}
+
+export default Footer;
